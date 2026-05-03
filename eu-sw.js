e1be785a9eb930110f8bee6194fbd88f7390ce83
robertmelc <robert.melc@gmail.com>
2026-05-03 15:47:00 +0200

    Add files via upload

---
+++ eu-sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'eu-solar-map-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
